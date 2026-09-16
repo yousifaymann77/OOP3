32fdd04c614199c0b6377f7ee72dd930ea61d543
yousif <yshehab693@gmail.com>
2026-09-16 11:13:17 +0300

    part01 Q1 a) What is the difference between Method Overloading and Method Overriding?

--- oop3Assign/Program.cs
+++ oop3Assign/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-
+            #region part01 Q1 a) What is the difference between Method Overloading and Method Overriding?
+             // Method overloading changes in the parameters of the Method ( in the number of params , it's data type or its order , And it is a static polymorphism (compile time poly)
+             // Method overriding used in inheritance between classes when a base class lets the derived one to change the Implementation of the method , And it is a Dynamic poly (Runtime poly)
+            #endregion
         }
     }
 }
