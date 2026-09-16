2e719591eb93eff40e1fc8ba218bf5511fd980f5
yousif <yshehab693@gmail.com>
2026-09-16 13:30:49 +0300

    part02 Q6 Delivery Helper Class

---
+++ oop3Assign/DeliveryHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop3Assign
+{
+    #region part02 Q6 Delivery Helper Class
+    internal static class DeliveryHelper
+    {
+        public static void PrintShipmentDetails(Shipment shipment)
+        {
+            shipment.PrintShipment();
+        }
+    }
+} 
+    #endregion
