4037e70feee992c24188aafaabb2eab852b3bb77
yousif <yshehab693@gmail.com>
2026-09-16 11:17:11 +0300

    Part01 Q1 b)What is the difference between Static Binding and Dynamic Binding?

--- oop3Assign/Program.cs
+++ oop3Assign/Program.cs
@@ -5,8 +5,13 @@
         static void Main(string[] args)
         {
             #region part01 Q1 a) What is the difference between Method Overloading and Method Overriding?
-             // Method overloading changes in the parameters of the Method ( in the number of params , it's data type or its order , And it is a static polymorphism (compile time poly)
-             // Method overriding used in inheritance between classes when a base class lets the derived one to change the Implementation of the method , And it is a Dynamic poly (Runtime poly)
+            // Method overloading changes in the parameters of the Method ( in the number of params , it's data type or its order , And it is a static polymorphism (compile time poly)
+            // Method overriding used in inheritance between classes when a base class lets the derived one to change the Implementation of the method , And it is a Dynamic poly (Runtime poly)
+            #endregion
+
+            #region Part01 Q1 b)What is the difference between Static Binding and Dynamic Binding?
+            // Static binding is performed By the compiler at the compile time and is decided by the Refrence type 
+            // Dynamic Binding is performed By the CLR at the Runtime and is decided by the Actual object in Memory 
             #endregion
         }
     }
